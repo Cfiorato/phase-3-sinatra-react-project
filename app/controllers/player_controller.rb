edcd1dc61d5e79a66b02e4e40307dade46ccd357
Cameron Fiorato <cameronclayfiorato@gmail.com>
2022-01-26 21:20:31 -0600

    Post for players

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -6,4 +6,13 @@ class PlayerController < ApplicationController
   get '/players/:id' do
     Player.find(params[:id]).to_json(include: :stats)
   end
+
+  post '/players' do 
+    Player.create(
+      name: params[:name],
+      jersey_number: params[:jersey_number],
+      position: params[:position],
+      team_id: params[:team_id]
+      img: params[:img]
+    )
 end
